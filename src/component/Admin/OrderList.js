b0f5705e958f59dd37f8d188c9d6e3f0b6ee2c1f
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-07-27 03:49:31 +0500

    21 commit

--- src/component/Admin/OrderList.js
+++ src/component/Admin/OrderList.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import { DataGrid } from "@material-ui/data-grid";
+// import { DataGrid } from "@material-ui/data-grid";
 import "./productList.css";
 import { useSelector, useDispatch } from "react-redux";
 import { Link,useNavigate } from "react-router-dom";
