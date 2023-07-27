453260ab1ff42c5b568fa57cf8079f4018dbaaf4
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-07-28 00:58:57 +0500

    40 commit

--- src/component/Manager/NewProductt.jsx
+++ src/component/Manager/NewProductt.jsx
@@ -3,7 +3,7 @@ import "./newProduct.css";
 import { useSelector, useDispatch } from "react-redux";
 import { clearError, createProduct } from "../../actions/productAction";
 import { useAlert } from "react-alert";
-import { Button } from "@material-ui/core";
+// import { Button } from "@material-ui/core";
 import Metatitle from "../title/title";
 import SideBar from "./Sidebar";
 import { useNavigate } from "react-router-dom";
