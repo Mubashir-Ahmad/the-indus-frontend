052426b876d4b331353d41fb5d1ed862cbc830a5
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-08-03 02:04:34 +0500

    130 commit

--- src/component/user/Login.jsx
+++ src/component/user/Login.jsx
@@ -55,7 +55,7 @@ dispatch(load_user())
 
   const loginSubmit = (e) => {
     e.preventDefault();
-    console.log('login authentication' ,isAuthenticated ,user)
+    console.log('login authentication' ,isAuthenticated ,user.user.role)
     dispatch(login(loginEmail, loginPassword));
     // Check if the login was successful and user is authenticated
     console.log('1  login authentication' ,isAuthenticated ,user)
