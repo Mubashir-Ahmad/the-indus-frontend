8a20f2d6d3c0bc2d816979620743d42ec825887f
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-08-04 01:18:59 +0500

    170 commit

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -6,6 +6,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Cartt from '../cart/Cartt';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 function Navbar() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -43,7 +51,44 @@ function Navbar() {
     transition: "transform 0.6s ease", // Add transition property for smooth transformation
   };
   const [keywords, setkeywords] = useState('');
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+             
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+             
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
   return (
     <>
       <div className='main-container' style={{ backgroundColor: '#ce1710' }}>
@@ -67,7 +112,6 @@ function Navbar() {
           </div>
           <div className='buttonn'>
             <Link className='linnk' onClick={togglePopupp}>
-            <i class="fa-solid fa-cart-shopping" style={{ color: cartitems.length > 0 ? 'tomato' : 'unset' }}></i>
               {/* <ShoppingCartIcon style={{ color: cartitems.length > 0 ? 'tomato' : 'unset' }} /> */}
               <span>{`(${cartitems.length})`}</span>
             </Link>
@@ -80,6 +124,18 @@ function Navbar() {
                 </div>
               </div>
             )}
+             {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
             {isAuthenticated ? null : (
               <Link to='/login' className='linnk'>
                 Sign in
