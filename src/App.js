9b22ae9047f5224dd97aea52120d3fd7e4099ee1
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-08-03 22:45:15 +0500

    162 commit

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
               <Route path='/password/reset/:token' element={<Resetpassword/>} />
               {/* {isAuthenticated && <Route path='/admindashbord' element={<Dashbord/>} /> : navigate('/login') } */}
               
-              {isAuthenticated &&  <Route path='/admindashbord' element={<Dashbord />} />  }
+              {isAuthenticated ?(  <Route path='/admindashbord' element={<Dashbord />} />) :(<Route path="/login" element={<Login />} exact />)  }
               <Route path='/admin/orders' element={<OrderList/>} />
               <Route path='/admin/products' element={<ProductList/>} />
               <Route path='/admin/users' element={<UserList/>} />
