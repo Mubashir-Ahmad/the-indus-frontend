32bed1d53c68441b1846732730b28ccef2659a00
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-08-04 01:30:21 +0500

    171 commit

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -50,7 +50,18 @@ function Navbar() {
     right: 0,
     transition: "transform 0.6s ease", // Add transition property for smooth transformation
   };
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+
+
+
   const [keywords, setkeywords] = useState('');
+  
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
